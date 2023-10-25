1fe57b434f558e250eb67f3bd4e7b5fe09fb01d0
Masrafi404 <mdmasrafi902@gmail.com>
2023-10-25 18:00:33 +0600

    carousel update

--- src/Components/Home/Navbar/Carusel.jsx
+++ src/Components/Home/Navbar/Carusel.jsx
@@ -36,13 +36,6 @@ const Carusel = () => {
                 </div>
             </div>
             <div className='relative'>
-                <img src="image12.jpg" />
-                <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
-                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
-                    <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
-                </div>
-            </div>
-            <div className='relative'>
                 <img src="image13.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
@@ -50,35 +43,49 @@ const Carusel = () => {
                 </div>
             </div>
             <div className='relative'>
-                <img src="image14.jpg" />
+                <img src="image22.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
-                <img src="image15.jpg" />
+                <img src="image23.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
-                <img src="image16.jpg" />
+                <img src="image24.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
-                <img src="image17.jpg" />
+                <img src="image25.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
-                <img src="image18.jpg" />
+                <img src="image26.jpg" />
+                <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
+                    <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
+                </div>
+            </div>
+            <div className='relative'>
+                <img src="image27.jpg" />
+                <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
+                    <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
+                </div>
+            </div>
+            <div className='relative'>
+                <img src="image28.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
                     <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for The Hajj and Umrah</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>

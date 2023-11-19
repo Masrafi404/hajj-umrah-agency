6d835bf7ba5a68c600594ffc893f4aa5ab5904a6
Masrafi404 <mdmasrafi902@gmail.com>
2023-11-19 15:46:21 +0600

    text shadaw added

--- src/Components/Home/Navbar/Carusel.jsx
+++ src/Components/Home/Navbar/Carusel.jsx
@@ -36,9 +36,9 @@ const Carusel = () => {
                 </div>
             </div>
             <div className='relative'>
-                <img src="image22.jpg" />
+                <img className='' src="image22.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
-                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for Travel in Dubai</p>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4 shadow-text'>Solution for Travel in Dubai</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
@@ -52,21 +52,21 @@ const Carusel = () => {
             <div className='relative'>
                 <img src="image24.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
-                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for Travel in Luxembourg</p>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4 shadow-text'>Solution for Travel in Luxembourg</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
                 <img src="image25.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
-                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for Travel in Maldives</p>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4 shadow-text'>Solution for Travel in Maldives</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
             <div className='relative'>
                 <img src="image26.jpg" />
                 <div className='absolute md:bottom-96 bottom-14 left-0 right-0 z-10 text-white'>
-                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4'>Solution for Travel in Thailand</p>
+                    <p className='font-bold md:text-[1.7rem] text-[1.5rem] sm:mx-4 shadow-text'>Solution for Travel in Thailand</p>
                     <button className='uppercase text-[13px] text-black bg-yellow-300 px-5 py-3 rounded-3xl'>apply now</button>
                 </div>
             </div>
